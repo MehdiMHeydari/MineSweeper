8127663d27c0566160627d8c565c1e5294577b59
mehdi <mehdi79h@gmail.com>
2020-04-18 16:39:40 -0700

    buggy nearly done

--- app/src/main/java/com/example/minesweeper/BoardLogic.java
+++ app/src/main/java/com/example/minesweeper/BoardLogic.java
@@ -9,10 +9,12 @@ public class BoardLogic {
     double percentmines;
 
 
-    public BoardLogic(int rows, int collumns, String difficulty) {
+    public BoardLogic(int row, int collumn, String difficulty) {
+
+
+        rows = row;
+        collumns = collumn;
         board = new int[rows][collumns];
-        rows = rows;
-        collumns = collumns;
         percentmines = 0;
         if (difficulty.equals("easy")) {
             percentmines = .15;
@@ -39,6 +41,14 @@ public class BoardLogic {
     }
 
     public int numOpen() {
+        numSafe = 0;
+        for (int i = 0; i < board.length; i++){
+           for(int j = 0; j< board[0].length; j++){
+               if(board[i][j]< 0 && board[i][j] == -9){
+                   numSafe ++;
+               }
+           }
+        }
         return numSafe;
     }
 
@@ -46,53 +56,93 @@ public class BoardLogic {
         return gameOver;
     }
 
+
     public int getSpace(int row, int col) {
-        return board[row][col];
+        if(board[row][col] == -9 ||board[row][col] == 9 ){
+            return 9;
+        } else if (board[row][col]< 0){
+            return -1;
+        } else {
+            return board[row][col];
+        }
     }
 
     public void reset() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < collumns; j++) {
-                board[i][j] = 0;
+                board[i][j] = -10;
                 addMines(i, j);
-                if (board[i][j] != 9) {
-                    populateBoard(i, j);
-                }
 
             }
         }
+        populateBoard();
     }
 
     private void addMines(int i, int j) {
         double x = Math.random();
+
         if (x < percentmines) {
-            board[i][j] = 9;
+            board[i][j] = -9;
         }
     }
 
-    private void populateBoard(int i, int j) {
-        int tally = 0;
-        for (int h = i - 1; h < i + 1; h++) {
-            for (int m = j; m < j + 1; m++) {
-                if (board[h][m] == 9) {
-                    tally++;
+    private void populateBoard() {
+        for (int h = 0; h < board.length; h++) {
+            for (int m = 0; m < board[1].length; m++) {
+                if (board[h][m] == -9) {
+                    for (int l = h -1; l <= h+ 1; l++){
+                        for (int u = m -1 ; u <= m +1; u++){
+                            try{
+                                if(board[l][u] != -9){
+                                    if(board[l][u] == -10){
+                                        board[l][u] = 0;
+                                    }
+                                    board[l][u]--;
+                                }
+                            }catch(ArrayIndexOutOfBoundsException e){
+                            }
+                            }
+                        }
+
+                    }
+                System.out.println(board[h][m]);
                 }
             }
-        }
-        board[i][j] = -1 * tally;
-    }
 
-    public void pickSpace(int i, int j) {
-        if (board[i][j] != 0) {
-            board[i][j] *= -1;
-        } else {
+        }
+
+
+
+    public boolean pickSpace(int i, int j) {
+
+        System.out.println(board[i][j]);
+        if (board[i][j] == -9){
+            gameOver = true;
+        }
+        if (isGameOver()){
+            return false;
+        }
+
+        if (board[i][j] != -10) {
+            if (board[i][j] < 0) {
+                board[i][j] *= -1;
+            }
+
+        } else if( board[i][j] == -10) {
             for (int h = i - 1; h < i + 1; h++) {
-                for (int m = j; m < j + 1; m++) {
-                    board[h][m] *= -1;
+                    for (int m = j - 1; m < j + 1; m++) {
+                        try {
+                            if(board[h][m] == -10){
+                                board[h][m] = 0;
+                                pickSpace(h,m);
+                            }
+                        }catch (IndexOutOfBoundsException c) {
+                        }
+                    }
                 }
-
             }
-
+        return true;
         }
+
     }
-}
+
--- app/src/main/java/com/example/minesweeper/GameBoardActivity.java
+++ app/src/main/java/com/example/minesweeper/GameBoardActivity.java
@@ -2,10 +2,12 @@ package com.example.minesweeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +17,11 @@ public class GameBoardActivity extends AppCompatActivity {
     int collumns;
     Chronometer cmTimer;
     MainActivity main;
+    BoardLogic logic;
+    Button[][] buttons;
+    TextView numopen;
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameboard);
@@ -33,10 +40,20 @@ public class GameBoardActivity extends AppCompatActivity {
         cmTimer.start();
 
         //make 2d array holding buttons
-        Button[][] buttons = new Button [rows][collumns];
+        buttons = new Button [rows][collumns];
+
+        logic = new BoardLogic(rows, collumns, difficulty);
+        logic.reset();
 
         //making game board
         linearLayout = findViewById(R.id.gameboard);
+
+        //linking textview variable
+        numopen = findViewById(R.id.numspaces);
+
+
+
+
         //first for loop puts in a linear layout in each rows
         for (int j = 0; j < rows; j++) {
             LinearLayout row = new LinearLayout(this);
@@ -47,6 +64,7 @@ public class GameBoardActivity extends AppCompatActivity {
             for(int h = 0; h< collumns; h++){
                 Button b = new Button(this);
 
+
                 LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
                 params.weight = 1;
                 b.setLayoutParams(params);
@@ -54,7 +72,18 @@ public class GameBoardActivity extends AppCompatActivity {
                 b.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        //main.updateGraphics();
+                        Button now = (Button)v;
+                        int tag = (Integer)now.getTag();
+                        logic.pickSpace(tag/rows, tag % collumns);
+                        System.out.println("getspace = " + logic.getSpace(tag/rows,tag% collumns));
+
+                        Handler handler = new Handler();
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateGraphics();
+                            }
+                        });
                     }
                 });
                 buttons[j][h] = b;
@@ -66,7 +95,7 @@ public class GameBoardActivity extends AppCompatActivity {
 
 
 
-        //puts the percent of the mines depending on diffuclty
+        //puts the percent of the mines depending on difficlty
         double percentmines = 0;
         if (difficulty.equals("easy")){
             percentmines = .15;
@@ -80,8 +109,29 @@ public class GameBoardActivity extends AppCompatActivity {
 
 
 
+    }
 
 
+    public void updateGraphics() {
+
+        numopen.setText(String.valueOf(logic.numOpen()));
+        if(logic.isGameOver()){
+            cmTimer.stop();
+            if (logic.numOpen() == 0){
+                numopen.setText("YOU WIN!");
+            }else{
+                numopen.setText("YOU LOST :(");
+            }
+        }
+
+
+        for (int i = 0; i < buttons.length; i++) {
+            for (int j = 0; j < buttons[1].length; j++) {
+                if(logic.getSpace(i,j) >= 0) {
+                    buttons[i][j].setText(String.valueOf(logic.getSpace(i, j)));
+                }
+            }
+        }
 
     }
 
